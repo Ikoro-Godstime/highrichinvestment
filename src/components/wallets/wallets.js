2bf9c94643e4a86ccb97a82f5afc19cc7b17c02b
theWarLock <fecypher@gmail.com>
2022-09-07 18:59:23 +0100

    changed the wallet address

--- src/components/wallets/wallets.js
+++ src/components/wallets/wallets.js
@@ -1,66 +1,24 @@
 import BtcLogo from "../../img/btc.png";
 import ethLogo from "../../img/eth.png";
-import ltcLogo from "../../img/ltc.png";
 import usdtLogo from "../../img/usdt.png";
-import bnbLogo from "../../img/bnb.png";
-import dogeLogo from "../../img/doge.png";
-import tronLogo from "../../img/tron.png";
-import shibaLogo from "../../img/shiba.png";
-import btccLogo from "../../img/btcc.png";
 
 export const wallets = [
   {
     coin: "Bitcoin",
-    address: "bc1q5uxe8mr4ctffc8wyg0jtuueszuej3aw72y4adk",
+    address: "1J1HrJt4fAUESGpikoYi8WSjUUKqjQqQMm",
     addressName: "BTC Address",
     icon: BtcLogo,
   },
   {
     coin: "Ethereum ",
-    address: "0xF7Dbe4a00630874B94D7D5ae2A8735d1398a5AA3",
+    address: "0x770fc50c070fe51aa3c816289dbb9d715ee75920",
     addressName: "ETH Address:",
     icon: ethLogo,
   },
   {
-    coin: "Litecoin",
-    address: "ltc1q4ghgj47qd822292dl90f00hte275yx8fmcs4mq",
-    addressName: "LTC Address",
-    icon: ltcLogo,
-  },
-  {
     coin: "USDT",
-    address: "TStrfndMyftHEmkyPeSb852kXvtcveHTsu",
+    address: "TFHnJ1YZ4rHaoRLaSqNcJ7tk8BEBkk1yP9",
     addressName: "USDT Address",
     icon: usdtLogo,
-  },
-  {
-    coin: "BNB",
-    address: "bnb10nqychwd78thk88nlpytzpdl4fls78mvfhmkzr",
-    addressName: "BNB Address",
-    icon: bnbLogo,
-  },
-  {
-    coin: "DOGE ",
-    address: "DRSy7UAqRz5ZkwAXFYvn1qydy2Ugiqdxy7",
-    addressName: "Dogecoin Address",
-    icon: dogeLogo,
-  },
-  {
-    coin: "TRON ",
-    address: "TStrfndMyftHEmkyPeSb852kXvtcveHTsu",
-    addressName: "TRON Address",
-    icon: tronLogo,
-  },
-  {
-    coin: "SHIBA",
-    address: "0xF7Dbe4a00630874B94D7D5ae2A8735d1398a5AA3",
-    addressName: "SHIBA Address",
-    icon: shibaLogo,
-  },
-  {
-    coin: "BITCOIN CASH",
-    address: "qqkcahum4ma9c97hwjcrfje8up9p3ksmxyperqc255",
-    addressName: "BCH Address",
-    icon: btccLogo,
   },
 ];
